4298074ba3f0d0586ac3db548ffda863ab85b725
John Larson <jlarson@idbydna.com>
2019-08-09 16:55:34 -0600

    put requirements in setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,25 @@
 import setuptools
 
-with open("README.md", "r") as fh:
+with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="hello",
-    version="0.2.3",
-    author="John",
-    author_email="jlarson@idbydna.com",
-    description="Says 'Hello, World'.",
+    name='hello',
+    version='0.2.3',
+    author='John',
+    author_email='jlarson@idbydna.com',
+    description='Says 'Hello, World'.',
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/johnlarson/hello_conda",
+    long_description_content_type='text/markdown',
+    url='https://github.com/johnlarson/hello_conda',
     packages=setuptools.find_packages(),
+    install_requires=[
+        'numpy==1.17.0',
+        'sympy==1.4',
+    ],
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
     ],
 )
