ffc536204c468e218cef908bc817a64e8a376ac9
John Larson <jlarson@idbydna.com>
2019-08-09 13:37:24 -0600

    python package install

---
+++ README.md
@@ -0,0 +1,11 @@
+This makes the difficult task of printin "Hello, World!" to the terminal very easy.
+
+No longer do you have to type in `print('Hello, World!')`.
+
+All you have to do is `conda install -c idbydna hello`, and write the following python code:
+
+```
+from hello import say_hello
+
+say_hello()
+```
---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="hello",
+    version="0.0.1",
+    author="John",
+    author_email="jlarson@idbydna.com",
+    description="Says 'Hello, World'.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/johnlarson/hello_conda",
+    packages=['hello'],#setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
